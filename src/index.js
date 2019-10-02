b8abc4c75c1f70d43114bd4c8ed0d6909ec6f1a3
Sean Yim <superjjang89@gmail.com>
2019-10-02 09:55:19 -0400

    bootstrapping

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from './reducers/rootReducer';
+import reduxPromise from 'redux-promise';
 
+const theStore = applyMiddleware(reduxPromise)(createStore)(rootReducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store = {theStore}>
+        <App/>
+    </Provider>, 
+    document.getElementById('root'));
 
