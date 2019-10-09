4bcc38614080113440806e1937a12f6b614afbdb
Sean Yim <superjjang89@gmail.com>
2019-10-09 15:09:57 -0400

    working account update

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -3,13 +3,18 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
+import './account.css';
 
 
 class Account extends Component {
     render() { 
         console.log(this.props);
         const {displayname, imageUrl, bannerUrl, profile} = this.props.auth;
+        console.log(bannerUrl);
         const bannerImage = `${window.apiHost}${bannerUrl}`;
+        const profileImage = `${window.apiHost}${imageUrl}`;
+
+        console.log(bannerImage);
         
 
         return (<>
@@ -26,7 +31,7 @@ class Account extends Component {
                     <div className="profileContainer row">
                         <div className="col s3">
                                 <div className="profileuploader">
-                            <img src="https://secure.gravatar.com/avatar/e711f05f912d97a74f5860381c6f57ef?r=r&s=256&d=https://s3.amazonaws.com/challonge_app/misc/challonge_fireball_gray.png" alt="profile" className="circle left"/>
+                            <img src={profileImage} alt="profile" className="circle left"/>
                             <div className="profileName left">{displayname}</div>
                                 {/* <input id="profile-image" type="file" ref="fileUploader" accept="image/*" style={{display:"none"}} onChange={this.changeProfileUrl}/> */}
                         {/* <Button onClick={this.onButtonClick} variant="contained" component="span" className="btner right">+</Button> */}
@@ -35,17 +40,15 @@ class Account extends Component {
                         </div>
                         <div className="col s9 left">
                             <div className="description">
-                                <div className="input-field col s12">
-                                    {/* <textarea id="textarea2" className="materialize-textarea" data-length="400" onChange={this.changeDescription}  value={this.state.profile} ></textarea> */}
-                                    {/* <label htmlFor="textarea2">400 Character limit</label> */}
+                                
                                     {profile}
-                                    {imageUrl}
-                                </div>
+                                    
+                                
                             </div>
                         </div>
                     </div>
                 </div>
-            <Link to="/my-account/edit"> something </Link>
+            <Link to="/my-account/edit"> Update your profile </Link>
             </div>
             
         </>);
